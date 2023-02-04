dfe67335d3269e1e5ede077a22ab1ec5daee1df9
mahdi halloum <mahdinah98@gmail.com>
2023-02-04 17:58:14 +0200

    finish contact us page without responsive

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,74 +2,118 @@ import React from "react";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
 import "../../styles/Contact.css";
-import contactus from "../../images/contactus.jpg";
+// import calldoctor from "../../images/calldoctor.jpeg";
 import chatus from "../../images/chatus.png";
 import callus from "../../images/callus.png";
 import visitus from "../../images/visitus.png";
-import { Whatsup }from "../whatsapp/whatsapp";
+import { Whatsup } from "../whatsapp/whatsapp";
+// import clinicjpg from "../../images/clinic.jpg";
 
 export const Contact = () => {
   return (
-
     (<Header />),
     (<Footer />),
-   
     (
       <section className="contactus-sec1">
-        <div className="middlenav">     
-       <Whatsup />,
-          <img src={contactus} alt="contactus" className="contactuspng"></img>
-        </div>
-        <div className="rightnav">
-          <div className="chatus">
-            <img src={chatus} alt="chatus" className="chatuspng"></img>
-            <div className="chato">
-            <h1>Chat with us</h1>
-            <h5>We are available 24/7 feel free to send a message</h5>
-            </div>
-          </div>
-          <div className="callus">
-            <img src={callus} alt="callus" className="calluspng"></img>
-            <div className="callo">
-            <h1>Call Us</h1>
-            <h5>don't waste time , feel free to call us </h5>
-            </div>
-          </div>
-          <div className="visitus">
-            <img src={visitus} alt="visitus" className="visituspng"></img>
-            <div className="visito">
-            <h1>Visit us</h1>
-            <h5>Our location is in beirut - Downtown near to azarieh .</h5>
-            </div>
-          </div>
-        </div>
-        <div className="leftnav">
-          <div className="fname">
-            <label>First Name</label>
-            <input></input>
-          </div>
+        <div className="clinic">
+          <div className="middlenav"></div>
+          <div className="middlectc">
+            <div class="card">
+              <h2>
+                Contact Us
+                <br />
+                we are always available for you
+              </h2>
+              <div class="row">
+                <div class="col">
+                  <div class="form-group">
+                    {/* <label>First Name</label> */}
+                    <input type="text" placeholder="First Name" />
+                  </div>
+                </div>
 
-          <div className="lname">
-            <label>Last Name</label>
-            <input></input>
-          </div>
+                <div class="col">
+                  <div class="form-group">
+                    {/* <label>Surname</label> */}
+                    <input type="text" placeholder="Surname" />
+                  </div>
+                </div>
 
-          <div className="email">
-            <label>Email</label>
-            <input />
+                <div class="col">
+                  <div class="form-group">
+                    {/* <label>Email</label> */}
+                    <input type="text" placeholder="Email" />
+                  </div>
+                </div>
+
+                <div class="col">
+                  <div class="form-group">
+                    {/* <label>Phone</label> */}
+                    <input type="text" placeholder="Phone Number" />
+                  </div>
+                </div>
+
+                <div class="col">
+                  <div class="form-group">
+                    {/* <label>Message</label> */}
+                    <textarea placeholder="Enter your message"></textarea>
+                  </div>
+                </div>
+
+                <div class="col">
+                  <input type="submit" value="Submit" />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="Phone Number">
-            <label>Phone Number</label>
-            <input></input>
-          </div>
-          <div className="messagewtsp">
-            <label>Message</label>
-            <input className="wtsinmput"></input>
-            <div className="wtspbttn">
-              <button>Send Whatsapp Message</button>
+          {/* <img src={clinicjpg} alt="clinic" className="clinicjpg"/> */}
+
+          <div className="rightnav">
+            <div className="chatus">
+              <img src={chatus} alt="chatus" className="chatuspng"></img>
+              <div className="chato">
+                <h1>Chat with us</h1>
+                <h5>We are available 24/7 feel free to send a message</h5>
+              </div>
+            </div>
+            <div className="callus">
+              <img src={callus} alt="callus" className="calluspng"></img>
+              <div className="callo">
+                <h1>Call Us</h1>
+                <h5>
+                  don't waste time , feel free to call us our doctors ready for
+                  you{" "}
+                </h5>
+              </div>
+            </div>
+            <div className="visitus">
+              <img src={visitus} alt="visitus" className="visituspng"></img>
+              <div className="visito">
+                <h1>Visit us</h1>
+                <h5>Our location is in beirut - Downtown near to azarieh .</h5>
+              </div>
             </div>
           </div>
         </div>
+
+       
+
+                <div className="dctorcall">
+
+                </div>
+
+      
+        <div className="bordermid">
+        <div className="Map">
+            <iframe
+              title="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26496.124608916696!2d35.48677272141671!3d33.88925268661702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f17215880a78f%3A0x729182bae99836b4!2sBeirut!5e0!3m2!1sen!2slb!4v1674946163797!5m2!1sen!2slb"
+              style={{ border: "0" }}
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+                </div></div>
+        <Whatsup />
       </section>
     )
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,52 +6,90 @@ import twit from "../../images/twitter.png";
 import yout from "../../images/youtube.png";
 export const Footer = () => {
   return (
-    <footer>
-      <div className="footer-bar-sec2">
-      <h2>Connect With Us</h2>
-        <ul className="socialicon">
-          <li>
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img className="facebook" src={faceb} alt="facebook"></img>
+    <footer className="footerpage">
+      <div class="main">
+        <div class="logo row">
+          <div class="footer-header">
+            <img
+              // src={twit}
+              class="Debtal-Solution"
+              alt=""
+            />
+          </div>
+          <div class="logo-des">
+            <p>
+              Established in 1977 | Newest technologies in Dentistry | 3D
+              digital impressions. 20 years warranty on dental implants | 10
+              years warranty of ceramic crowns.
+            </p>
+
+            <a href="www.Debtalsolution.com" class="btn-know">
+              Know More
             </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img className="instagram" src={insta} alt="instagram"></img>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com/" target="_blank" rel="noreferrer">
-              <img className="twitter" src={twit} alt="twitter"></img>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
-              <img className="youtube" src={yout} alt="youtube"></img>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="footer-bar-sec4">
-        <div className="Map">
-          <iframe
-            title="map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26496.124608916696!2d35.48677272141671!3d33.88925268661702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f17215880a78f%3A0x729182bae99836b4!2sBeirut!5e0!3m2!1sen!2slb!4v1674946163797!5m2!1sen!2slb"
-            style={{ border: "0" }}
-            allowfullscreen=""
-            loading="lazy"
-          ></iframe>
+          </div>
         </div>
+
+        <div class="office row">
+          <div class="footer-header">
+            <h3>Office</h3>
+          </div>
+          <div class="office-des">
+            <p>Now we have 6 branches for our dentist in all lebanon</p>
+
+            <a href="www.google.com">Solution-Dentist@gmail.com</a>
+
+            <p class="num">+961-03643397</p>
+          </div>
+        </div>
+
+        <div class="link row">
+          <div class="footer-header">
+            <h2>Links</h2>
+          </div>
+
+          <div class="link-des">
+            <ul>
+              <a href="www.twitter.com" class="footer-links">
+                <img src={twit} alt="twitter" className="icomedia"></img>
+                {/* Home */}
+              </a>
+              <a href="www.google.com" class="footer-links">
+                <img src={yout} alt="youtube" className="icomedia"></img>
+                {/* About */}
+              </a>
+              <a href="www.google.com" class="footer-links">
+                <img src={insta} alt="instagram" className="icomedia"></img>
+                {/* Services */}
+              </a>
+              <a href="www.google.com" class="footer-links">
+                <img src={faceb} alt="facebook" className="icomedia"></img>
+                {/* Galary */}
+              </a>
+              {/* <a href="www.google.com" class="footer-links">
+              Contact
+            </a> */}
+            </ul>
+          </div>
+        </div>
+
+        <div class="newsletter row">
+          <div class="footer-header">
+            <h3>Our Beirut Branch</h3>
+          </div>
+
+          <div className="Mapi">
+            <iframe
+              title="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26496.124608916696!2d35.48677272141671!3d33.88925268661702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f17215880a78f%3A0x729182bae99836b4!2sBeirut!5e0!3m2!1sen!2slb!4v1674946163797!5m2!1sen!2slb"
+              style={{ border: "0" }}
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
+        
+        </div>
+    
       </div>
-      {/*  */}
     </footer>
   );
 };
