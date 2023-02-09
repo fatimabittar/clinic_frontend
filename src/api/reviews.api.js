7e015a32d2009328de7e6cf6d2a8ccdaf9f5013c
mahdi halloum <mahdinah98@gmail.com>
2023-02-09 18:35:45 +0200

    mahdi update footer

--- src/api/reviews.api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const domain = "http://localhost:8000";
-
-export const getReviews = async () => await axios.get(`${domain}/reviews/read`);
-
-export const addReview = (item) => axios.post(`${domain}/reviews/add`, item);
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,124 +1,50 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import logo from "../../images/logo.png";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { useLogout } from "../../hooks/useLogout";
-import { useAuthContext } from "../../hooks/useAuthContext";
 import "../../styles/Header.css";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export const Header = () => {
   const path = window.location.pathname;
   const [openMenu, setOpenMenu] = useState(false);
-  const nav = useNavigate();
-  const { user } = useAuthContext();
-  const { logout } = useLogout();
-  const handleClick = () => {
-    setOpenMenu(false);
-    logout();
-    nav("/");
-  };
+
   return (
     <header>
       <a href="/">
         <img src={logo} alt="logo" className="logoheader" />
       </a>
+      <hr1 />
       <ul className={`${openMenu === true ? "nav-open" : "nav-closed"}`}>
         <li className="animate">
-          <Link
-            to="/"
-            className={path === "/" ? "current" : ""}
-            onClick={() => setOpenMenu(false)}
-          >
+          <a href="/" className={path === "/" ? "current" : ""}>
             Home
-          </Link>
+          </a>
         </li>
         <li className="animate">
-          <Link
-            to="/about"
-            className={path === "/about" ? "current" : ""}
-            onClick={() => setOpenMenu(false)}
-          >
+          <a href="/about" className={path === "/about" ? "current" : ""}>
             About
-          </Link>
+          </a>
         </li>
         <li className="animate">
-          <Link
-            to="/services"
-            className={path === "/services" ? "current" : ""}
-            onClick={() => setOpenMenu(false)}
-          >
+          <a href="/services" className={path === "/services" ? "current" : ""}>
             Services
-          </Link>
+          </a>
         </li>
         <li className="animate">
-          <Link
-            to="/reviews"
-            className={path === "/reviews" ? "current" : ""}
-            onClick={() => setOpenMenu(false)}
-          >
+          <a href="/reviews" className={path === "/reviews" ? "current" : ""}>
             Reviews
-          </Link>
+          </a>
         </li>
         <li className="animate">
-          <Link
-            to="/contact"
-            className={path === "/contact" ? "current" : ""}
-            onClick={() => setOpenMenu(false)}
-          >
+          <a href="/contact" className={path === "/contact" ? "current" : ""}>
             Contact
-          </Link>
+          </a>
         </li>
-        {user && (
-          <li>
-            <Link
-              to="/dashboard"
-              className={path === "/dashboard" ? "current" : ""}
-              onClick={() => setOpenMenu(false)}
-            >
-              Dashboard
-            </Link>
-          </li>
-        )}
         <li className="left">
-          <button
-            onClick={() => {
-              setOpenMenu(false);
-              nav("/", { state: { targetId: "appointment" } });
-            }}
-            className={openMenu ? "secondary-button" : "primary-button"}
-          >
-            Appointment
-          </button>
+          <button className="primary-button">Appointment</button>
         </li>
-        {user && (
-          <li className="left">
-            <button onClick={handleClick} className="secondary-button">
-              Logout
-            </button>
-          </li>
-        )}
-        {!user && (
-          <li className="left">
-            <div>
-              <button
-                onClick={() => {
-                  setOpenMenu(false);
-                  nav("/login");
-                }}
-                className="secondary-button"
-              >
-                Login
-              </button>
-            </div>
-          </li>
-        )}
-
-        <button
-          onClick={() => setOpenMenu(!openMenu)}
-          className="nav-btn nav-close-btn"
-        >
-          {openMenu ? <FaTimes /> : <FaBars />}
-        </button>
+      <button onClick={() => setOpenMenu(!openMenu)} className="nav-btn nav-close-btn">
+        {openMenu ? <FaTimes />: <FaBars />}
+      </button>
       </ul>
     </header>
   );
